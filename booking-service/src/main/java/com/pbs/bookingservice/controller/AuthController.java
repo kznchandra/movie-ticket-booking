eab3ab1964c5a2b50605f45b17a3917619f073ee
kaizen <code.chandrashekharv@gmail.com>
2026-01-28 22:32:16 +0530

    auth api changes

--- booking-service/src/main/java/com/pbs/bookingservice/controller/AuthController.java
+++ booking-service/src/main/java/com/pbs/bookingservice/controller/AuthController.java
@@ -1,7 +1,12 @@
 package com.pbs.bookingservice.controller;
 
 import com.pbs.bookingservice.service.AppService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth API", description = "API for authentication operations")
 public class AuthController {
     private final AppService appService;
+
+    @Operation(summary = "Get JWT token", description = "Returns JWT token for authenticated user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "JWT token retrieved successfully"),
+    })
     @GetMapping("/token")
-    public String getToken() {
-        return appService.getJwtToken();
+    public ResponseEntity<String> getToken() {
+        return ResponseEntity.ok(appService.getJwtToken());
     }
 }
